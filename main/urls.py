8fd0314b79b78aa17d5fb2b3013582652eaceb7c
emilte <emil.telstad@gmail.com>
2020-04-05 22:40:48 +0200

    Fix

--- main/urls.py
+++ main/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('distanse', include('distance'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
