3e965dd68b8caae800209683bbdc734afdceafe7
emilte <emil.telstad@gmail.com>
2020-04-06 12:35:10 +0200

    urls fix

---
+++ patrick/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'patrick'
+
+urlpatterns = [
+    #path('registrering/', views.Register.as_view(), name='register'),
+]
+
+
+# localhost:8000/distanse/registrering
