b5ab98cd95814282a927472dd4a5067ba9b0b019
emilte <emil.telstad@gmail.com>
2020-04-06 21:25:10 +0200

    fix unique distance url bug

--- henrik/urls.py
+++ henrik/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'distance'
+app_name = 'henrik'
 
 urlpatterns = [
     #path('registrering/', views.Register.as_view(), name='register'),
